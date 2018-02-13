9276748188c4480ade2db6663e1ea8d4f7990b14
Chandan Jha <jha2011eec43@gmail.com>
2018-02-13 11:32:59 +0530

    Added initialization fun

--- esp4.c
+++ esp4.c
@@ -44,7 +44,47 @@ static void esp_destroy(struct xfrm_state *x)
 
 static int esp_init_state(struct xfrm_state *x)
 {
-	return 0;
+	struct crypto_aead *aead;
+	u32 align;
+	int err;
+	x->data = NULL;
+
+	if (x->aead)
+		err = esp_init_aead(x);
+	else
+		err = esp_init_authenc(x);
+
+	if (err)
+		goto error;
+
+	aead = x->data;
+	x->props.header_len = sizeof(struct ip_esp_hdr) +         /* esp header size + ivsize initialization vector is the size of a block */
+			      crypto_aead_ivsize(aead);
+	if (x->props.mode == XFRM_MODE_TUNNEL)
+		x->props.header_len += sizeof(struct iphdr);
+	else if (x->props.mode == XFRM_MODE_BEET && x->sel.family != AF_INET6)
+		x->props.header_len += IPV4_BEET_PHMAXLEN;
+	
+	if (x->encap) {
+		struct xfrm_encap_tmpl *encap = x->encap;
+
+		switch (encap->encap_type) {
+		default:
+			goto error;
+		case UDP_ENCAP_ESPINUDP:
+			x->props.header_len += sizeof(struct udphdr);
+			break;
+		case UDP_ENCAP_ESPINUDP_NON_IKE:
+			x->props.header_len += sizeof(struct udphdr) + 2 * sizeof(u32);
+			break;
+		}
+	}
+//TODO
+	align = ALIGN(crypto_aead_blocksize(aead), 4);
+	x->props.trailer_len = align + 1 + crypto_aead_authsize(aead);
+
+error:
+	return err;
 }
 static int esp4_rcv_cb(struct sk_buff *skb, int err)
 {
